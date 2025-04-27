500c3cef772a7a999467ea1ab657d92a38203fee
GitKodland <125306926+GitKodland@users.noreply.github.com>
2025-04-27 13:13:06 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -14,13 +14,13 @@ class Pokemon:
             self = Pokemon.pokemons[pokemon_trainer]
 
     async def get_name(self):
-        # PokeAPI aracılığıyla bir pokémonun adını almak için eşzamansız bir yöntem
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API'si
-        async with aiohttp.ClientSession() as session:  # Bir HTTP oturumu açma
+        # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
             async with session.get(url) as response:  # GET isteği gönderme
                 if response.status == 200:
-                    data = await response.json()  # JSON yanıtının alınması ve kodunun çözülmesi
-                    return data['forms'][0]['name']  # Bir Pokémon'un adını döndürme
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    return data['forms'][0]['name']  #  Pokémon adını döndürme
                 else:
                     return "Pikachu"  # İstek başarısız olursa varsayılan adı döndürür
 
@@ -28,7 +28,7 @@ class Pokemon:
         # Pokémon hakkında bilgi döndüren bir metot
         if not self.name:
             self.name = await self.get_name()  # Henüz yüklenmemişse bir adın geri alınması
-        return f"Pokémonunuzun ismi: {self.name}"  # Pokémon'un adını içeren dizeyi döndürür
+        return f"Pokémonunuzun ismi: {self.name}"  # Pokémon adını içeren dizeyi döndürür
 
     async def show_img(self):
-        # PokeAPI aracılığıyla bir pokémon görüntüsünün URL'sini almak için eşzamansız bir yöntem
+        # PokeAPI aracılığıyla bir pokémon görüntüsünün URL'sini almak için asenktron metot
